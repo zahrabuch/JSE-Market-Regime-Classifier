5bbdb0984420640b6a5c392bb413b9d781f62507
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2026-07-09 20:39:35 -0500

    cleaning data and establishing features: SMA

---
+++ market_regime/data.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("JSE All Jamaican Composite Historical Data.csv")
+
+print(df.info())
+
+df["Change %"] = df["Change %"].str.replace("%", "", regex=False)
+
+num_col = ["Price", "Open", "High", "Low", "Change %"]
+
+for col in num_col:
+    df[col] = df[col].str.replace(",", "", regex=False)
+    df[col] = pd.to_numeric(df[col], errors="coerce")
+
+print(df.info())
+
+def convert_volume(volume):
+    if pd.isna(volume):
+        return np.nan
+
+    volume = str(volume).replace(",", "").strip().upper()
+
+    if volume.endswith("M"):
+        return float(volume[:-1]) * 1_000_000
+
+    elif volume.endswith("K"):
+        return float(volume[:-1]) * 1_000
+
+    elif volume.endswith("B"):
+        return float(volume[:-1]) * 1_000_000_000
+
+    return float(volume)
+
+df["Vol."] = df["Vol."].apply(convert_volume)
+
+def numerize_data(df, num_col):
+    for col in num_col:
+        pd.to_numeric(df[col], errors="coerce")
+
+    return df
+
+df["Date"] = pd.to_datetime(df["Date"])
+df = df.sort_values("Date").reset_index(drop=True)
+df = df.set_index("Date")
+df["Daily Return"] = df["Price"].pct_change()
+print(df.info())
+df = df.rename(columns={"Price": "Close"})
+
+#FEATURES
+df["SMA20"] = df["Close"].rolling(20).mean()
+df["SMA50"] = df["Close"].rolling(50).mean()
+df["SMA200"] = df["Close"].rolling(200).mean()
+
+df["Dist_SMA20"] = (df["Close"] - df["SMA20"]) / df["SMA20"]
+df["Dist_SMA50"] = (df["Close"] - df["SMA50"]) / df["SMA50"]
+df["Dist_SMA200"] = (df["Close"] - df["SMA200"]) / df["SMA200"]
