7a9e34c6c9c66db30dc476846a454286a05d133d
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2026-07-11 22:50:38 -0500

    reformatting

--- market_regime/data.py
+++ market_regime/data.py
@@ -15,6 +15,11 @@ for col in num_col:
 
 print(df.info())
 
+
+def load_csv():
+    return pd.read_csv("JSE All Jamaican Composite Historical Data.csv")
+
+
 def convert_volume(volume):
     if pd.isna(volume):
         return np.nan
@@ -32,13 +37,30 @@ def convert_volume(volume):
 
     return float(volume)
 
+
 df["Vol."] = df["Vol."].apply(convert_volume)
 
-def numerize_data(df, num_col):
+
+def clean_data(df):
+    df["Change %"] = df["Change %"].str.replace("%", "", regex=False)
+
+    num_col = ["Price", "Open", "High", "Low", "Change %"]
+
     for col in num_col:
-        pd.to_numeric(df[col], errors="coerce")
+        df[col] = df[col].str.replace(",", "", regex=False)
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    df["Vol."] = df["Vol."].apply(convert_volume)
+
+    df["Date"] = pd.to_datetime(df["Date"])
+    df = df.sort_values("Date").reset_index(drop=True)
+    df = df.set_index("Date")
+
+    df["Daily Return"] = df["Price"].pct_change()
+    df = df.rename(columns={"Price": "Close"})
 
     return df
+
 
 df["Date"] = pd.to_datetime(df["Date"])
 df = df.sort_values("Date").reset_index(drop=True)
@@ -47,11 +69,14 @@ df["Daily Return"] = df["Price"].pct_change()
 print(df.info())
 df = df.rename(columns={"Price": "Close"})
 
-#FEATURES
-df["SMA20"] = df["Close"].rolling(20).mean()
-df["SMA50"] = df["Close"].rolling(50).mean()
-df["SMA200"] = df["Close"].rolling(200).mean()
 
-df["Dist_SMA20"] = (df["Close"] - df["SMA20"]) / df["SMA20"]
-df["Dist_SMA50"] = (df["Close"] - df["SMA50"]) / df["SMA50"]
-df["Dist_SMA200"] = (df["Close"] - df["SMA200"]) / df["SMA200"]
+#FEATURES
+def create_features(df):
+    df["SMA20"] = df["Close"].rolling(20).mean()
+    df["SMA50"] = df["Close"].rolling(50).mean()
+    df["SMA200"] = df["Close"].rolling(200).mean()
+
+    df["Dist_SMA20"] = (df["Close"] - df["SMA20"]) / df["SMA20"]
+    df["Dist_SMA50"] = (df["Close"] - df["SMA50"]) / df["SMA50"]
+    df["Dist_SMA200"] = (df["Close"] - df["SMA200"]) / df["SMA200"]
+
